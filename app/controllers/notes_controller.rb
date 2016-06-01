1402507f886e2d3594ceb22581c340cd3ce4b95a
Mary Griffus <mary.e.shultz@gmail.com>
2016-06-01 10:52:15 -0400

    add to controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,4 +4,29 @@ class NotesController < ApplicationController
     @notes = Note.all
     render json: @notes.to_json, status: :ok
   end
+
+  def create
+    @note = @note.build(note_params)
+    if @note.save
+      render json: @note.to_json, status: :created
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      render json: @note.to_json, status: :ok
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    render json: {message: "success"}, status: :ok
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :author, :body, :station, :date)
+  end
 end
